a6d46ebbecd3bd9db67454bd35fe4e1b4423fa85
Hamim <t.i.m.hamim@gmail.com>
2024-02-03 00:03:10 +0600

    updated

--- Python_Basic/Backslash/Escape_Characters_in_Strings.py
+++ Python_Basic/Backslash/Escape_Characters_in_Strings.py
@@ -1,5 +1,5 @@
 print("Hamim Talukdar\nJim talukdar") # \n --> Newline
 print("Pagla\tCode") # \t --> Tab
-print("Hamim\\Minian")
-print("Hamim\'s Dragon")
-print("\"Why so serious\"")
+print("Hamim\\Minian") # \\ --> Backslash
+print("Hamim\'s Dragon") # \' --> Single quote
+print("\"Why so serious\"") # \" --. Double quote
