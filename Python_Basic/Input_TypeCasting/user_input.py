7627d5bc3ccd79c7819141ece91c88adb4594a9c
Hamim <t.i.m.hamim@gmail.com>
2024-02-03 01:54:56 +0600

    user input and type casting added

---
+++ Python_Basic/Input_TypeCasting/user_input.py
@@ -0,0 +1,11 @@
+# input() function reads a line from the user and returns it as a string
+
+# Taking user input
+user_input = input("Enter something: ")
+
+# Displaying the input
+print("You entered:", user_input)
+
+
+a = input()
+print("a = ", a)
---
+++ Python_Basic/Input_TypeCasting/user_input_typeCasting.py
@@ -0,0 +1,11 @@
+# Taking integer input
+num = int(input("Enter an integer: "))
+print("You entered:", num)
+
+# Taking floating-point input
+float_num = float(input("Enter a float: "))
+print("You entered:", float_num)
+
+
+a = int(input())
+print("a = ", a)
