91eb5cc104738c6c00afeb995ae87629a5afcdc6
Hamim <t.i.m.hamim@gmail.com>
2024-02-02 22:51:05 +0600

    comments added

---
+++ Python_Basic/Comments/comments.py
@@ -0,0 +1,14 @@
+# This is a single-line comment
+print("Hello, World!")  # This is another single-line comment
+
+'''
+This is a multi-line comment
+spanning multiple lines.
+'''
+print("Hello, World!")
+
+"""
+Another way to create
+a multi-line comment.
+"""
+print("Hello again!")
