4c581e05998fbccd643f52f1fff0d859647b3655
Hamim <t.i.m.hamim@gmail.com>
2024-02-03 23:36:25 +0600

    break and continue statement added

---
+++ Python_Basic/Break_Continue/break.py
@@ -0,0 +1,6 @@
+numbers = [1, 2, 3, 4, 5]
+
+for num in numbers:
+    if num == 3:
+        break  # Exit the loop when num == 3
+    print(num)
---
+++ Python_Basic/Break_Continue/continue.py
@@ -0,0 +1,6 @@
+numbers = [1, 2, 3, 4, 5]
+
+for num in numbers:
+    if num == 3:
+        continue  # Skip the rest of the code for num == 3
+    print(num)
