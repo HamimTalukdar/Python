60f3f3486dba89041e24b6b3bd852503f39729e1
Hamim <t.i.m.hamim@gmail.com>
2024-02-05 00:52:45 +0600

    stack added

---
+++ Python_DSA/Stack/Implementing_a_Stack_using_a_List.py
@@ -0,0 +1,17 @@
+# Creating an empty stack
+stack = []
+
+# Pushing elements onto the stack
+stack.append(1)
+stack.append(2)
+stack.append(3)
+
+# Displaying the stack
+print("Stack:", stack)
+
+# Popping elements from the stack
+popped_element = stack.pop()
+print("Popped Element:", popped_element)
+
+# Displaying the updated stack
+print("Updated Stack:", stack)
