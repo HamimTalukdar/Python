d0a6dd84434ee89039dd081512b7dcfabd8eae23
Hamim <t.i.m.hamim@gmail.com>
2024-02-06 16:22:45 +0600

    zip function added

---
+++ Python_Basic/Function/Zip_function.py
@@ -0,0 +1,8 @@
+roll = [101, 102, 103, 104, 105]
+name = ["Anisul Islam", "Hamim Talukdar", "Jim Islam", "Hridi", "Mithu"]
+
+result = list(zip(roll, name))
+print(result)
+
+result = list(zip(roll, name, "ABCDE"))
+print(result)
